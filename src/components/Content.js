81fbfd5a2ab120175132bd759f6a4d1bbfcd754c
elitemoni <a.flying.cucumber@gmail.com>
2023-08-25 21:23:13 -0700

    education + css changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,12 +4,14 @@ import Profile from './Profile.js';
 import Bio from './Bio.js';
 import Skills from './Skills.js';
 import Projects from './Projects.js';
+import Education from './Education.js';
 
 const Content = () => {
   return (
     <div className="content-container">
       <Profile />
       <Bio />
+      <Education />
       <Skills />
       <Projects />
     </div>
---
+++ src/components/Education.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Education = () => {
+  return (
+    <div className="education-container">
+      <div>Education:</div>
+      <div>Associate's Degree in Computer Science from Diablo Valley College</div>
+    </div>
+  )
+}
+
+export default Education
