b3efaa2cce1dba6b1f204fbfb409435f5d13d6be
elitemoni <a.flying.cucumber@gmail.com>
2023-08-26 15:51:17 -0700

    anchor test

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import TableOfContents from './TOC.js';
 function App() {
   return (
     <div className="App">
+      <a href="elitemoni.github.io/CellularAutomata/">Click</a>
       <Content />
     </div>
   );
