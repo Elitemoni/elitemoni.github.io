045e530b42ea746d85972b42f30e383ffc70dcf6
Elitemoni <cekovsimeon1@gmail.com>
2025-06-06 23:58:32 -0700

    filled out the content, will do a redesign

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,11 @@
 export default function Project() {
    return (
-      <div>Project</div>
+      <div className="w-126 h-126 flex justify-center items-center">
+         <iframe 
+            className="w-126 h-126"
+            title="Cellular Automata"
+            src="https://elitemoni.github.io/CellularAutomata/"
+         />
+      </div>
    )
 }
